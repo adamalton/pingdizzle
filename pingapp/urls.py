509316ab2397debb947aab065b65494ab6246cc7
Adam Alton <adam@potatolondon.com>
2013-04-26 10:29:20 +0100

    Protect all the views that do the pinging to prevent them being abused.

--- pingapp/urls.py
+++ pingapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('pingapp.views',
     url(r"^$", "index"),
-    url(r"^spawn_pings/([A-Z]+)/$", "spawn_pings", name="spawn_pings"),
-    url(r"^do_ping/$", "do_ping", name="do_ping"),
+    url(r"^protected/spawn_pings/([A-Z]+)/$", "spawn_pings", name="spawn_pings"),
+    url(r"^protected/do_ping/$", "do_ping", name="do_ping"),
 )
